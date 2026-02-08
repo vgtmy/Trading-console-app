fb4c7d170c614bd91ded2aa116bec2afdc1b935b
vgtmy <vgtmy@126.com>
2026-02-08 11:38:46 +0800

    Add custom Document component for Next.js

---
+++ pages/_document.tsx
@@ -0,0 +1,17 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#111111" />
+        <link rel="apple-touch-icon" href="/icon-192.png" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
